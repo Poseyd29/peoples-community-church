cb6bbaba74ec9726c766ab5bae4abceb2baed5cd
Poseyd29 <poseyd29@gmail.com>
2022-07-19 22:21:56 -0400

    node.js mailchimp pt1
    got mailchimp connected to server. newsletter form is able to submit information to mailchimp audience list. still need to figure out if i should serve all files with express static

--- app.js
+++ app.js
@@ -1,17 +1,67 @@
 const express = require('express');
 const request = require('request');
 const path = require('path');
+const bodyParser = require('body-parser')
 
 const app = express();
 
 
 
 
-
+// Body Parser
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // STATIC FOLDER
 app.use(express.static(path.join(__dirname, 'public')))
 
+// Signup Route
+app.post('/signup', (req, res) => {
+    const { firstName, lastName, email } = req.body;
+    //make sure fields are filled
+    if (!firstName || !lastName || !email) {
+        res.redirect('/fail.html')
+        return;
+    }
+
+    // Construct Request Data
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: 'subscribed',
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    }
+
+    const postData = JSON.stringify(data)
+
+    const options = {
+        url: 'https://us14.api.mailchimp.com/3.0/lists/6f9b962e65',
+        method: 'POST',
+        headers: {
+            Authorization: 'auth 128217c07dac0a6153ed796a3511e3bc-us14'
+        },
+        body: postData
+    }
+
+    request(options, (err, response, body) => {
+        if (err) {
+            res.redirect('/fail.html')
+        } else {
+            if (response.statusCode === 200) {
+                res.redirect('/success.html')
+            } else {
+                res.redirect('/fail.html')
+            }
+        }
+    });
+})
+
 const PORT = process.env.PORT || 5300;
 
 app.listen(PORT, console.log(`server started on ${PORT}`));
+
